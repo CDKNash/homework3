6f3a0a3e3b8976feb04a0b555d2a80a81c1612de
CDKNash <cdknash123@gmail.com>
2024-06-30 16:45:34 +0400

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,12 @@
+name = 'Ruslan'
+print(name, type(name))
+print('Name:', name)
+age = 32
+print(age, type(age))
+print('Age:', age)
+new_age = age + 100
+print(new_age)
+print('New_age:', new_age)
+is_student = True
+print(is_student)
+print('Is_Student:', is_student)
