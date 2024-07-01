0745b3022fcc53483830138e43eb2cba5a6e7310
CDKNash <cdknash123@gmail.com>
2024-07-01 09:40:35 +0400

    Update homework3.py

--- homework3.py
+++ homework3.py
@@ -1,12 +1,7 @@
 name = 'Ruslan'
-print(name, type(name))
-print('Name:', name)
+print('Name:', name, type(name))
 age = 32
-print(age, type(age))
-print('Age:', age)
-new_age = age + 100
-print(new_age)
-print('New_age:', new_age)
+print('Age:', age, type(age))
+print('New_age:', age + 10)
 is_student = True
-print(is_student)
 print('Is_Student:', is_student)
